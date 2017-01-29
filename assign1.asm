8d01274403c0680b83584aee1776899cc59daa0f
arsatvik <rohansatvik98@gmail.com>
2017-01-29 14:38:06 +0530

    Update assign1.asm

--- assign1.asm
+++ assign1.asm
@@ -1,40 +1,38 @@
 .data
-
+    input: .space 1000              # allocate memory to store input string
 
 .text
 .globl main
 .ent main
 
 main:
-    li $v0 , 5              # input integer x
+    la $a0 , input                  # $a0 stores address of string
+    li $a1 , 1000
+    li $v0 , 8                      # read string
     syscall
-    add $s0 , $v0 , $zero   # store x in $s0
 
-    li $v0 , 5              # input integer y
-    syscall
-    add $s1 , $v0 , $zero   # store y in $s1
+    li $s0 , 0                      # $s0 is loop counter (initialised here)
+Loop:
+    add $s1 , $s0 , $a0             # $s1 = address of current character
+    lb $s2 , 0($s1)                 # $s2 = ASCII value of the character
 
-    mul $t0 , $s0 , -5      # $t0 = $s0 * -5  [$t0 = -5 * x]
-    mul $t1 , $s1 , -7      # $t1 = $s1 * -7  [$t1 = -7 * y ]
-    add $s2 , $t0 , $t1     # $s2 = $t0 + $t1 [u = -5 * x  +  -7 * y]
+    beq $s2 , $zero , EXIT          # exit loop if null character is found
 
-    slti $t0 , $s2 , -35    # if u($s2) < -35
-    bne $t0 , $zero , L1    # go to L1
+    slti $t0 , $s2 , 123            # current char < 'z'
+    beq $t0 , $zero , L1            # go to L1 if not
 
-    slti $t0 , $s2 , 35     # if u($s2) not < 35
-    beq $t0 , $zero , L2    # go to L2
+    slti $t1 , $s2 , 97             # current char < 'a'
+    bne $t1 , $zero , L1            # go to L1 if true
 
-    add $a0 , $s2 , $zero   # else $a0 = u($s2)
-    j EXIT                  # go to exit
+    addi $t2 , $s2 , -32            # convert to upper case
+    sb $t2 , 0($s1)                 # store converted in memory
 
 L1:
-    li $a0 , -35            # set $a0 (output) = -35
-    j EXIT
-L2:
-    li $a0 , 35             # set $a0 = 35
-    j EXIT
+    addi $s0 , $s0 , 1              # increment counter
+    j Loop                          # loop
+
 EXIT:
-    li $v0 , 1              # print output ($a0)
+    li $v0 , 4                      # print (converted)  string
     syscall
 
     li $v0 , 10
