65eafd8f045afad852da95232990c668e6d21770
arsatvik <rohansatvik98@gmail.com>
2017-01-29 14:39:06 +0530

    Update assign1.asm

--- assign1.asm
+++ assign1.asm
@@ -1,40 +1,53 @@
 .data
-    input: .space 1000              # allocate memory to store input string
+
 
 .text
 .globl main
 .ent main
 
 main:
-    la $a0 , input                  # $a0 stores address of string
-    li $a1 , 1000
-    li $v0 , 8                      # read string
+    li $v0 , 5              # input integer x
     syscall
+    add $a0 , $v0 , $zero   # store x in $a0
 
-    li $s0 , 0                      # $s0 is loop counter (initialised here)
-Loop:
-    add $s1 , $s0 , $a0             # $s1 = address of current character
-    lb $s2 , 0($s1)                 # $s2 = ASCII value of the character
+    li $v0 , 5              # input integer y
+    syscall
+    add $a1 , $v0 , $zero   # store y in $a1
 
-    beq $s2 , $zero , EXIT          # exit loop if null character is found
+    jal func                # function call
+    add $a0 , $v0 , $zero   # store output of func in ($a0)
 
-    slti $t0 , $s2 , 123            # current char < 'z'
-    beq $t0 , $zero , L1            # go to L1 if not
-
-    slti $t1 , $s2 , 97             # current char < 'a'
-    bne $t1 , $zero , L1            # go to L1 if true
-
-    addi $t2 , $s2 , -32            # convert to upper case
-    sb $t2 , 0($s1)                 # store converted in memory
-
-L1:
-    addi $s0 , $s0 , 1              # increment counter
-    j Loop                          # loop
-
-EXIT:
-    li $v0 , 4                      # print (converted)  string
+    li $v0 , 1              # print output ($a0)
     syscall
 
     li $v0 , 10
     syscall
 .end main
+
+.globl func
+.ent func
+func:
+
+mul $t0 , $a0 , -5      # $t0 = $a0 * -5  [$t0 = -5 * x]
+mul $t1 , $a1 , -7      # $t1 = $a1 * -7  [$t1 = -7 * y ]
+add $v0 , $t0 , $t1     # $v0 = $t0 + $t1 [u = -5 * x  +  -7 * y]
+
+slti $t0 , $v0 , -35    # if u($v0) < -35
+bne $t0 , $zero , L1    # go to L1
+
+slti $t0 , $v0 , 35     # if u($v0) not < 35
+beq $t0 , $zero , L2    # go to L2
+
+                        # else output u($v0)
+j EXIT                  # go to exit
+
+L1:
+li $v0 , -35            # set $v0 (output) = -35
+j EXIT
+L2:
+li $v0 , 35             # set $v0 (output) = 35
+j EXIT
+
+EXIT:
+  jr $ra
+.end func
